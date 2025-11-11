76992016229ca98af9766878aa9ef8ff22a11494
Shahriar262 <srh.2622@gmail.com>
2025-11-11 13:04:30 +0600

    server is connected to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config()
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+ `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.akvcfmt.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Social development event server is running");
