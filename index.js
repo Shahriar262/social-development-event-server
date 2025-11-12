1c891b5bc0478df1689e39a4533d90e9a11d82dc
Shahriar262 <srh.2622@gmail.com>
2025-11-13 03:27:21 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("event-db");
     const eventCollection = db.collection("events");
@@ -164,7 +164,7 @@ async function run() {
       });
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
